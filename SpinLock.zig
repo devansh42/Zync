8067e72e779902326dd89b6c345b55db39379305
Devansh Gupta <devanshguptamrt@gmail.com>
2026-01-09 01:08:01 +0530

    Create SpinLock.zig

---
+++ SpinLock.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const SpinLock = @This();
+
+locked: std.atomic.Value(u8),
+pub fn init() SpinLock {
+    return .{ .locked = std.atomic.Value(u8).init(0) };
+}
+pub fn lock(self: *SpinLock) void {
+    while (true) {
+        _ = (self.locked.cmpxchgWeak(0, 1, .acquire, .monotonic)) orelse return;
+    }
+}
+pub fn unlock(self: *SpinLock) void {
+    _ = self.locked.store(0, .release);
+}
